0facd4cc08d15bf0eac91166c1fd12d02047dcab
maris <maris.raukas@voco.ee>
2024-11-20 14:32:04 +0200

    test commit

---
+++ uniuhghbi.py
@@ -0,0 +1,28 @@
+"""Function examples."""
+
+
+# func()
+def func():
+    print("I´m inside the function")
+func()
+name=# my_name_is(name)
+def my_name_is(name):
+    print(f"my name is {name}")
+    return name
+my_name_is(name)
+
+# sum_six(num)`
+def sum_six(num):
+    return num + 6
+print(sum_six(9))
+# sum_numbers()
+def sum_numbers(a,b):
+    return a+b
+print(sum_numbers(8,8))
+
+
+
+
+# usd_to_eur()
+def usd_to_eur():
+return usd * 0.8
